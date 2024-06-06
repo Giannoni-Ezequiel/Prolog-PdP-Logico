a4c3479eb5fb667de8044ff81fb712d62e989754
Giannoni-Ezequiel <egiannoni@frba.utn.edu.ar>
2024-06-06 15:13:27 -0300

    ForAll

--- compras.pl
+++ compras.pl
@@ -3,10 +3,21 @@ compra(Persona, Producto, Cantidad):-
   precio(Producto, Precio),
   Cantidad is Dinero / Precio,
   Dinero >= Precio.
-
+%  // division entera
+% Vuelto: Dinero - Cantidad*Precio
 dinero(nicolas, 200).
 dinero(javier, 1030).
 
 precio(cafe, 25).
 precio(auriculares, 250).
 precio(mouse, 400).
+
+/*CONSULTAS
+compra(Alguien,Producto,Cant).  Existencialidad
+
+*/
+
+compraCualquierCosa(Alguien):-
+   dinero(Alguien, Dinero),
+   forall(precio(Producto,Precio), Dinero>=Precio).
+%Para todo producto que hay alcanza el dinero.
