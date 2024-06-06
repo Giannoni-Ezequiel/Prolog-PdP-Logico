b2c281a208db560e29dcef13caee851dffd4ba5f
Giannoni-Ezequiel <egiannoni@frba.utn.edu.ar>
2024-06-06 18:20:53 -0300

    Formas del Para Todo

--- compras.pl
+++ compras.pl
@@ -7,6 +7,7 @@ compra(Persona, Producto, Cantidad):-
 % Vuelto: Dinero - Cantidad*Precio
 dinero(nicolas, 200).
 dinero(javier, 1030).
+dinero(ezequiel, 100000).
 
 precio(cafe, 25).
 precio(auriculares, 250).
@@ -19,5 +20,18 @@ compra(Alguien,Producto,Cant).  Existencialidad
 
 compraCualquierCosa(Alguien):-
    dinero(Alguien, Dinero),
-   forall(precio(Producto,Precio), Dinero>=Precio).
+   forall(precio(Producto,Precio), 
+          Dinero>=Precio).
 %Para todo producto que hay alcanza el dinero.
+%Tener cuidado en que condiciones analizo (son dos)
+
+compraMasDeUnoDeCualquierCosa(Alguien):-
+  dinero(Alguien,Dinero),
+  forall(precio(Producto,Precio),
+        (compra(Alguien,Producto,Cantidad,Vuelto),Cantidad>1)
+        ).
+
+siempreQueCompraCompraVarios(Alguien):-
+  dinero(Alguien,Dinero),
+  forall((precio(Producto,Precio),compra(Alguien,Producto,Cantidad,Vuelto)),
+          Cantidad>1).
