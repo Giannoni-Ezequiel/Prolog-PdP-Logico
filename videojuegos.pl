c9b97e1e6de1e59d0834a8195f4c2ef73d3f76bc
Ezequiel <egiannoni@frba.utn.edu.ar>
2024-07-02 19:06:24 -0300

    Test Inversibilidad

--- videojuegos.pl
+++ videojuegos.pl
@@ -21,3 +21,12 @@ test(pokemonGo_no_es_un_videojuego, fail):-
     videojuego(pokemonGo).
 
 :- end_tests(videojuegos).
+
+:- begin_tests(generos).
+test(the_last_of_us_2_es_de_accion, nondet):-
+    genero(accion, theLastOfUs2).
+%Testear inversibilidad
+test(se_puede_saber_todos_los_juegos_de_accion, 
+     set(Juegos == [theLastOfUs2, doomEternal, jaja])):-
+    genero(accion, Juegos).
+:- end_tests(generos).
