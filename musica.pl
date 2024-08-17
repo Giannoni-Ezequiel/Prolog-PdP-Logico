7f5de41ee9725e25e1b1e96bb5104a784013c2a6
Giannoni-Ezequiel <egiannoni@frba.utn.edu.ar>
2024-08-17 15:11:18 -0300

    Create musica.pl

---
+++ musica.pl
@@ -0,0 +1,9 @@
+% disco(artista, nombreDelDisco, cantidad, año).
+disco(floydRosa, elLadoBrillanteDeLaLuna, 1000000, 1973).
+disco(tablasDeCanada, autopistaTransargentina, 500, 2006).
+disco(rodrigoMalo, elCaballo, 5000000, 1999).
+disco(rodrigoMalo, loPeorDelAmor, 50000000, 1996).
+disco(rodrigoMalo, loMejorDe, 50000000, 2018).
+disco(losOportunistasDelConurbano, ginobili, 5, 2018).
+disco(losOportunistasDelConurbano, messiMessiMessi, 5, 2018).
+disco(losOportunistasDelConurbano, marthaArgerich, 15, 2019).
